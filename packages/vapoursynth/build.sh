82f84c31fda24eeafd3a7ce11a05cfcff8cb0c42
Uranite <62639703+Uranite@users.noreply.github.com>
2025-05-24 15:50:18 +0700

    vapoursynth

---
+++ packages/vapoursynth/build.sh
@@ -0,0 +1,19 @@
+TERMUX_PKG_HOMEPAGE=https://www.vapoursynth.com/
+TERMUX_PKG_DESCRIPTION="A video processing framework with simplicity in mind"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=71
+TERMUX_PKG_SRCURL=https://github.com/vapoursynth/vapoursynth/archive/refs/tags/R${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=SKIP_CHECKSUM
+TERMUX_PKG_DEPENDS="python, libzimg"
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, 'Cython>=3.0.0'"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_configure() {
+	./configure --prefix=$PREFIX
+}
+
+termux_step_make_install() {
+	make install
+	pip install . --prefix=$PREFIX --no-deps --no-build-isolation
+}
